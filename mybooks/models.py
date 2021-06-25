76fdcaf7d060dd867be36b1ff2e24b9e44e5810c
Marcia Marques <marcia.marques@alumni.usp.br>
2021-06-25 16:54:44 -0300

    Create Book model

--- mybooks/models.py
+++ mybooks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    date = models.DateField(verbose_name="publishing date")
+    epub = models.FileField(upload_to='books/epubs/')
+    cover = models.ImageField(upload_to='books/covers/')
+
+    def __str__(self):
+        return self.title
--- mybooks/urls.py
+++ mybooks/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -7,3 +10,7 @@ urlpatterns = [
     # class based views
     path('class/', views.HomeView.as_view(), name='class_home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
